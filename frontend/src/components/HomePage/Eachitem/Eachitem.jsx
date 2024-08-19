a106db9dee402b7f8549b2c02753292fb3223bcf
Goggu <86672200+Goggu@users.noreply.github.com>
2024-08-19 12:24:38 +0530

    Update Eachitem.jsx

--- frontend/src/components/HomePage/Eachitem/Eachitem.jsx
+++ frontend/src/components/HomePage/Eachitem/Eachitem.jsx
@@ -1,21 +1,19 @@
+
 import React from 'react'
 import './Eachitem.css'
 
 const Eachitem = ({id,image,namee,owner}) => {
   return (
-    <div className='each-item'>
-      <div className='each-item-info'>
-        <div>
-          <p className='post-title'>{namee}</p>
-        </div>
-        <div>
-          <p className='post-owner'>{owner}</p>
-        </div>                
-      </div>
+    <div className='each-item'>      
+      <div>
+        <p className='post-title'>{namee}</p>
+      </div>                      
       <div className='each-item-img-container'>
         <img className='each-item-image' src={image} alt='' />
+      </div>      
+      <div>
+        <p className='post-owner'>{owner}</p>
       </div>
-      
     </div>
   )
 }
