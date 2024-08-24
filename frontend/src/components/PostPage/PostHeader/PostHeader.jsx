90048a056230699f5352e8ce8f5f40d421fd4edc
Goggu <86672200+Goggu@users.noreply.github.com>
2024-08-24 12:29:46 +0530

    Update PostHeader.jsx

--- frontend/src/components/PostPage/PostHeader/PostHeader.jsx
+++ frontend/src/components/PostPage/PostHeader/PostHeader.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import './PostHeader.css'
 
-const PostHeader = ({id,namee,owner,profilepic}) => {
+const PostHeader = ({id,namee,owner,profilepic,image}) => {
   return (
     <div className="header-container">
            
+      <div className="meta-info"> 
+        <img src={profilepic} alt='' />
+        <p>&nbsp;{owner}</p> 
+        <button className='follow-button' type="button">Follow</button>    
+      </div> 
+
       <h1 className="title">{namee}</h1>     
 
-      <div className="meta-info"> 
-        <p>{owner}</p>        
-        <p className='pipe'> | </p>
-        <p>Buisness</p>
-      </div>
-      
     </div>
   )
 }
