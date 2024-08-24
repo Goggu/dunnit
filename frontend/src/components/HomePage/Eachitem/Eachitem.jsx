05c08e9e77231247e7b015db609c07b26ea6f95a
Goggu <86672200+Goggu@users.noreply.github.com>
2024-08-24 12:24:00 +0530

    Update Eachitem.jsx

--- frontend/src/components/HomePage/Eachitem/Eachitem.jsx
+++ frontend/src/components/HomePage/Eachitem/Eachitem.jsx
@@ -1,20 +1,40 @@
 
 import React from 'react'
+import { useState } from 'react'
 import './Eachitem.css'
+import { assets } from '../../../assets/assets'
 
-const Eachitem = ({id,image,namee,owner,profilepic}) => {
+const Eachitem = ({ id, image, namee, owner, profilepic }) => {
+
+  const [isBookmarked, setIsBookmarked] = useState(false);
+
+  const toggleBookmark = () => {
+    setIsBookmarked(!isBookmarked);
+  };
+
   return (
-    <div className='each-item'>      
-      <div>
+    <div className='each-item'>            
+      <div className='title-container'>
         <p className='post-title'>{namee}</p>
-      </div>                      
+      </div>
       <div className='each-item-img-container'>
         <img className='each-item-image' src={image} alt='' />
-      </div>      
-      <div className='profile-info'>
-        <img className='profile-pic' src={profilepic} alt='' />
-        <p className='profile-name'>{owner}</p>
       </div>
+      <div className='each-item-bottom'>
+        <div className='profile-info'>
+          <img className='profile-pic' src={image} alt='' />
+          <p className='profile-name'>{owner}</p>        
+        </div>
+        <div>
+          <img 
+            className='bookmark-item-icon' 
+            src={ isBookmarked ? assets.save_article_icon_filled : assets.save_article_icon } 
+            alt='Bookmark'
+            onClick={toggleBookmark}
+            style={{ cursor:'pointer' }} 
+          />
+        </div>
+      </div>       
     </div>
   )
 }
