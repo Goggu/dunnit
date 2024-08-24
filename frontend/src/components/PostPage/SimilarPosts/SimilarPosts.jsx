81d94961596e819f10ec7d54d02bc4dd0863e89b
Goggu <86672200+Goggu@users.noreply.github.com>
2024-08-24 12:30:44 +0530

    Update SimilarPosts.jsx

--- frontend/src/components/PostPage/SimilarPosts/SimilarPosts.jsx
+++ frontend/src/components/PostPage/SimilarPosts/SimilarPosts.jsx
@@ -3,13 +3,14 @@ import './SimilarPosts.css'
 import { Link } from 'react-router-dom'
 import { StoreContext } from '../../../context/StoreContext'
 import EachSimilarPost from '../EachSimilarPost/EachSimilarPost'
+import { Box } from '@mui/material'
 
 const SimilarPosts = () => {
 
   const { post_list } = useContext(StoreContext)
 
   return (
-    <div className='similar-posts-list'>
+    <Box className='similar-posts-list'>
       
       {post_list.map((item) =>{
         return <Link to={`/article/${item._id}`} key={item._id}>     {/* Wrap Eachitem with Link */}
@@ -17,7 +18,7 @@ const SimilarPosts = () => {
         </Link>
       })}
 
-    </div>
+    </Box>
   )
 }
 
