62b86d3764232801f0fdd7440d787538f2c931c5
Goggu <86672200+Goggu@users.noreply.github.com>
2024-08-24 12:33:39 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,12 +1,14 @@
 import { createContext } from "react";
 import { post_list } from "../assets/assets";
+import { bookmarks } from "../assets/assets";
 
 export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) => {
 
     const contextValue = {
-        post_list
+        post_list,
+        bookmarks
     }
 
     return (
