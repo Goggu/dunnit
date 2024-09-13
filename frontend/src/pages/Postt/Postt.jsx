4455ea34fed38e9365da5b1f096cbedeb5d5598e
Goggu <86672200+Goggu@users.noreply.github.com>
2024-09-13 21:24:20 +0530

    Update Postt.jsx

--- frontend/src/pages/Postt/Postt.jsx
+++ frontend/src/pages/Postt/Postt.jsx
@@ -7,6 +7,7 @@ import PostContent from '../../components/PostPage/PostContent/PostContent'
 import SimilarPosts from '../../components/PostPage/SimilarPosts/SimilarPosts'
 import { useState } from 'react'
 import { assets } from '../../assets/assets'
+import { post_list } from '../../assets/assets'
 
 const Postt = () => {
 
@@ -28,17 +29,25 @@ const Postt = () => {
         
         <div className='current-article' component="main" sx={{ flexGrow: 1, p: 3, marginTop: -4, marginBottom: 2, marginRight: 0 }}>
           <div className='post-title' >
-            <PostHeader id={article._id} namee={article.namee} owner={article.owner} profilepic={article.profilepic} datee={article.datee} />
+            <PostHeader id={article._id} namee={article.namee} datee={article.datee} />
+          </div>
+          <div>
+            <hr style={{ border: 'none', borderTop: '1px solid #ddd', margin: '5px 0px 5px 30px', opacity: 0.8, width: '93%' }}/>
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <div className="meta-info"> 
+                <img src={assets.profilepic} alt='' />
+                <p>&nbsp;{post_list.owner}</p>
+              </div>
+              <div className='like-dislike-comment-share'>            
+                <img className='like-icon' src={assets.like_icon} alt='' />
+                <img className='share_icon' src={assets.share_icon} alt='' />
+                <img className='save-article-icon' src={assets.save_article_icon} alt='' />
+              </div>
+            </div>            
+            <hr style={{ border: 'none', borderTop: '1px solid #ddd', margin: '5px 0px 5px 30px', opacity: 0.8, width: '93%' }}/>
           </div>
           <div className='post-content'>
             <PostContent id={article._id} content={article.content} />
-          </div>  
-          <div className='like-dislike-comment-share'>            
-            <img className='like-icon' src={assets.like_icon} alt='' />
-            <img className='dislike-icon' src={assets.dislike_icon} alt='' />
-            <img className='comment-icon' src={assets.comment_icon} alt='' />
-            <img className='share_icon' src={assets.share_icon} alt='' />
-            <img className='save-article-icon' src={assets.save_article_icon} alt='' />
           </div>        
         </div>
         
