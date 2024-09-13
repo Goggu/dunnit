e545bb250f9e65438123a2f85af9e196afa79354
Goggu <86672200+Goggu@users.noreply.github.com>
2024-09-13 21:35:57 +0530

    Update EachSimilarPost.jsx

--- frontend/src/components/PostPage/EachSimilarPost/EachSimilarPost.jsx
+++ frontend/src/components/PostPage/EachSimilarPost/EachSimilarPost.jsx
@@ -4,9 +4,6 @@ import './EachSimilarPost.css';
 const EachSimilarPost = ({id,image,namee}) => {
   return (
     <div className='each-item-container'>
-      <div className='each-item-img-container'>
-        <img className='each-item-image' src={image} alt='' />
-      </div>
       <div className='each-item-info'>
         <p className='post-heading'>{namee}</p>
       </div>
